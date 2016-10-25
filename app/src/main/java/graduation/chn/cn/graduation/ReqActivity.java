449ae30211731de7f9914fc67864eb8165df3b8c
caihn <745677674@qq.com>
2016-10-25 14:47:05 +0800

    first commit

---
+++ app/src/main/java/graduation/chn/cn/graduation/ReqActivity.java
@@ -0,0 +1,13 @@
+package graduation.chn.cn.graduation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_req);
+    }
+}
---
+++ app/src/main/java/graduation/chn/cn/tomcat/TomcatActivity.java
@@ -0,0 +1,15 @@
+package graduation.chn.cn.tomcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import graduation.chn.cn.graduation.R;
+
+public class TomcatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tomcat);
+    }
+}
